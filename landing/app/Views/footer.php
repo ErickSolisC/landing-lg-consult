efc6c94be0cb16c16536e7707868a7499e495e95
ErickSolisC <ericksolis705@gmail.com>
2025-10-04 19:42:54 -0600

    Parametrizar perfiles linkedin y github

--- landing/app/Views/footer.php
+++ landing/app/Views/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="social-links">
-    <a href="https://linkedin.com" target="_blank" rel="noopener" aria-label="LinkedIn">LinkedIn</a>
-    <a href="https://github.com" target="_blank" rel="noopener" aria-label="GitHub">GitHub</a>
+    <a href="https://www.linkedin.com/in/erick-amado-solis-cuxún-2b091a222" target="_blank" rel="noopener" aria-label="LinkedIn">LinkedIn</a>
+    <a href="https://github.com/ErickSolisC" target="_blank" rel="noopener" aria-label="GitHub">GitHub</a>
   </div>
-  <p>&copy; <?php echo date("Y"); ?> ES. Todos los dereches del autor son reservados.</p>
+  <p>&copy; <?php echo date("Y"); ?> ES. Todos los derechos del autor son reservados.</p>
 </footer>
